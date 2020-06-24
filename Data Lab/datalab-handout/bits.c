986840c8e34d4589db14154c3cca4c80a456317c
Alpha-Girl <alphaboy@mail.ustc.edu.cn>
2020-06-24 16:59:18 +0800

    style

--- Data Lab/datalab-handout/bits.c
+++ Data Lab/datalab-handout/bits.c
@@ -293,9 +293,7 @@ int howManyBits(int x)
 {
   int sign, pos, bias;
   sign = x >> 31;
-  /* if negative, don't negate, just flip bits */
   x = (sign & (~x)) | (~sign & x);
-  /* bias=1 if x==0 */
   bias = !(x ^ 0);
   pos = (!!(x >> 16)) << 4;
   pos |= (!!(x >> (pos + 8))) << 3;
@@ -323,26 +321,19 @@ unsigned floatScale2(unsigned uf)
   unsigned frac = uf & 0x7FFFFF;
   if (exp == 0)
   {
-    /* Denormalized.  Must double fraction */
     frac = 2 * frac;
     if (frac > 0x7FFFFF)
     {
-      /* Result normalized */
-      frac = frac & 0x7FFFFF; /* Chop off leading bit */
+      frac = frac & 0x7FFFFF;
       exp = 1;
     }
   }
   else if (exp < 0xFF)
   {
-    /* Normalized.  Increase exponent */
     exp++;
     if (exp == 0xFF)
-    {
-      /* Infinity */
       frac = 0;
-    }
   }
-  /* Infinity and NaN do not require any changes */
   return (sign << 31) | (exp << 23) | frac;
 }
 /* 
@@ -362,31 +353,16 @@ int floatFloat2Int(unsigned uf)
   unsigned sign = uf >> 31;
   unsigned exp = (uf >> 23) & 0xFF;
   unsigned frac = uf & 0x7FFFFF;
-  /* Create normalized value with leading one inserted,
-     and rest of significand in bits 8--30.
-  */
   unsigned val = 0x80000000u + (frac << 8);
   if (exp < 127)
-  {
-    /* Absolute value is < 1 */
     return 0;
-  }
   if (exp > 158)
-    /* Overflow */
     return 0x80000000u;
-  /* Shift val right */
   val = val >> (158 - exp);
-  /* Check if out of range */
   if (sign)
-  {
-    /* Negative */
     return val > 0x80000000u ? 0x80000000u : -val;
-  }
   else
-  {
-    /* Positive */
     return val > 0x7FFFFFFF ? 0x80000000u : val;
-  }
 }
 /* 
  * floatPower2 - Return bit-level equivalent of the expression 2.0^x
@@ -403,32 +379,28 @@ int floatFloat2Int(unsigned uf)
  */
 unsigned floatPower2(int x)
 {
-  unsigned sign = uf >> 31;
-  unsigned exp = (uf >> 23) & 0xFF;
-  unsigned frac = uf & 0x7FFFFF;
-  /* Create normalized value with leading one inserted,
-     and rest of significand in bits 8--30.
-  */
-  unsigned val = 0x80000000u + (frac << 8);
-  if (exp < 127)
+  unsigned exp, frac;
+  unsigned u;
+  if (x < -149)
   {
-    /* Absolute value is < 1 */
-    return 0;
+    exp = 0;
+    frac = 0;
   }
-  if (exp > 158)
-    /* Overflow */
-    return 0x80000000u;
-  /* Shift val right */
-  val = val >> (158 - exp);
-  /* Check if out of range */
-  if (sign)
+  else if (x < -126)
   {
-    /* Negative */
-    return val > 0x80000000u ? 0x80000000u : -val;
+    exp = 0;
+    frac = 1 << (x + 149);
+  }
+  else if (x < 128)
+  {
+    exp = x + 127;
+    frac = 0;
   }
   else
   {
-    /* Positive */
-    return val > 0x7FFFFFFF ? 0x80000000u : val;
+    exp = 255;
+    frac = 0;
   }
+  u = exp << 23 | frac;
+  return u;
 }
